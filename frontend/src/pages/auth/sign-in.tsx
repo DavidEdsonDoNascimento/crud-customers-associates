2afa1e87a03a0fc59b8b9c6694e893ab1ca82bb5
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-06-14 23:01:03 -0300

    created login page with tailwindcss and validate with hook-form

--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -2,8 +2,24 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signForm = z.object({
+  email: z.string().email()
+});
+
+type SignForm = z.infer<typeof signForm>
 
 export const SignIn = () => {
+
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignForm>();
+
+  const handleSignIn = async (data: SignForm) => {
+    console.log(data.email);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -13,12 +29,12 @@ export const SignIn = () => {
             <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
             <p className="text-sm text-muted-foreground">CRUD feito para um desafio de entrevista</p>
           </div>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">E-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
-            <Button className="w-full" type="submit">Acessar</Button>
+            <Button className="w-full" type="submit" disabled={isSubmitting}>Acessar</Button>
           </form>
         </div>
       </div>
