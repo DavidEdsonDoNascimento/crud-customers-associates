e272fe6669eb908e6de729c9efd8b449293af678
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-06-14 22:44:24 -0300

    created login page with tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
-// import { RouterProvider } from 'react-router-dom';
-// import { router } from './routes';
-// import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { Button } from './components/ui/button';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import './global.css'
 
-export const App = ()  => {
+export const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Enviar</Button>
-    </div>
-    // <HelmetProvider>
-    //   <Helmet titleTemplate='%s | desafio'/>
-    //   <RouterProvider router={router} />
-    // </HelmetProvider>
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | desafio' />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   );
 }
--- frontend/src/pages/_layouts/auth.tsx
+++ frontend/src/pages/_layouts/auth.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom"
+import { Coffee } from "lucide-react"
 
 export const AuthLayout = () => {
   return (
     <div className="min-h-screen grid grid-cols-2">
-      <h1>Autenticação</h1>
-      <div>
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Coffee className="h-5 w-5" />
+          <span className="font-semibold">Desafio</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; Desafio - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
